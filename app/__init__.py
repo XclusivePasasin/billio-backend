b1224a90342ad26c1ecd4576c5fbf72596fb4ded
XclusivePasasin <elizar.rebollo23@itca.edu.sv>
2024-09-16 19:52:07 -0600

    New structure of project

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
-from .routes.users import users_bp  # Importar el blueprint de users
+from .models import db
+from .routes.user_route import users_bp
+from config import Config
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
 
-    # Registrar el blueprint de usuarios
+    # Inicializar la base de datos
+    db.init_app(app)
+
+    # Registrar las rutas
     app.register_blueprint(users_bp, url_prefix='/users')
 
     return app
--- config.py
+++ config.py
@@ -1,6 +1,15 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    
+    DB_HOST = os.getenv('DB_HOST')
+    DB_NAME = os.getenv('DB_NAME')
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS", "False").lower() in ["true", "1", "t"]
